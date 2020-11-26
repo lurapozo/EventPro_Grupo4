82802b1b65f6d4d2c90d882724c6b0776ce8c3a3
Lurapozo <lurapozo@espol.edu.ec>
2020-11-25 19:07:04 -0500

    Creacion de la clase NewEnum con los numTypes necesarios

---
+++ [FP4][PY1P]PrimerParcial_Ortiz_Pisco_Ramos/src/Eventos/NewEnum.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Eventos;
+
+/**
+ *
+ * @author luisr
+ */
+public enum NewEnum {
+}
+enum TipoEvento{
+    bodas, infantiles, empresariales
+}
+enum AdicionalTipo{
+    comida, bocaditos, bebida, musica, fotografia
+}
+enum EstadoEvento{
+    pendiente, aprovado
+}
+enum EstadoP{
+    PagoPendiente, Pagado
+}
+enum Transporte{
+    Aplica, NoAplica
+}
