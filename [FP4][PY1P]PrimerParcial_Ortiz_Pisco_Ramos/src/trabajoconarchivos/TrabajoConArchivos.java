fb5b4be65810232ff3af9903c5f943fdc951e9c6
Lurapozo <lurapozo@espol.edu.ec>
2020-11-27 19:08:10 -0500

    Se añadio las clases de lector de archivos

---
+++ [FP4][PY1P]PrimerParcial_Ortiz_Pisco_Ramos/src/trabajoconarchivos/TrabajoConArchivos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajoconarchivos;
+
+/**
+ *
+ * @author Verónica
+ */
+public class TrabajoConArchivos {
+
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] arg) {
+        ManejoArchivos.EscribirArchivo("archivosalida.txt","Hola!");
+        ManejoArchivos.EscribirArchivo("archivosalida.txt","Afios!");
+        ManejoArchivos.LeeFichero("archivo.txt");
+
+    }
+    
+}
